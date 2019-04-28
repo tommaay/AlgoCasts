945c544b0a6d954719362cfc8e3e55465a4efe8e
ToMMaaY <thuynh629@gmail.com>
2019-04-28 10:15:00 -0700

    completed max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let ch of str) {
+        chMap[ch] = chMap[ch] + 1 || 1; // if first case is undefined, set value to 1
+    }
+
+    for (let ch in chMap) {
+        if (chMap[ch] > max) {
+            max = chMap[ch];
+            maxChar = ch;
+        }
+    }
+
+    return maxChar;
+}
 
 module.exports = maxChar;
