9e2433915d9e67831cb1c5bb67acb397b781f1ff
ToMMaaY <thuynh629@gmail.com>
2019-04-30 15:51:02 -0700

    completed steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,23 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    // have a loop of n times that will console log the result
+    // have a nested loop that will create the result to log
+
+    for (let i = 0; i < n; i++) {
+        let result = '';
+
+        for (let j = 0; j < n; j++) {
+            if (j <= i) {
+                result += '#';
+            } else {
+                result += ' ';
+            }
+        }
+
+        console.log(result);
+    }
+}
 
 module.exports = steps;
