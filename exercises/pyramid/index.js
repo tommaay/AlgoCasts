58540e3678a51772933e3fb57e7f05a6e8d66efe
ToMMaaY <thuynh629@gmail.com>
2019-04-30 17:29:47 -0700

    completed pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -13,7 +13,42 @@
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)
+//      '   #   '
+//      '  ###  '
+//      ' ##### '
+//      '#######'
 
-function pyramid(n) {}
+function pyramid(n) {
+    // number of rows = n
+    // number of columns = n + 2
+    // number of spaces between = n - row
+    // rows go from 1 up to and including n
+
+    // if (row === 0) {
+    //     number of '#' per row = 1
+    // } else {
+    //     number of '#' per row = row * 2 - 1
+    // }
+
+    const rowLength = n * 2 - 1;
+
+    for (let row = 1; row <= n; row++) {
+        let temp = '';
+        const numSpaces = n - row;
+
+        for (let col = 0; col < rowLength; col++) {
+            if (col < numSpaces) {
+                temp += ' ';
+            } else if (col >= numSpaces && col < rowLength - numSpaces) {
+                temp += '#';
+            } else {
+                temp += ' ';
+            }
+        }
+
+        console.log(temp);
+    }
+}
 
 module.exports = pyramid;
